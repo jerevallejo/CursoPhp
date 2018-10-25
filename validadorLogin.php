15ee9e313cf76016d7a8dd0116d28cb6197f07a9
jerevallejo <44449584+jerevallejo@users.noreply.github.com>
2018-10-25 10:24:02 -0300

    login2

--- validadorLogin.php
+++ validadorLogin.php
@@ -5,23 +5,24 @@
 	<title>Document</title>
 </head>
 <body>
+<center>
 
 <?php
-$pasword="admin";
-$user="admin"; 
-$img='<img src="img/gandalf.jpg" >';
-$contraseña=$_POST["contraseña"];
-$usuario=$_POST["usuario"];
+	$pasword="admin";
+	$user="admin"; 
+	$img='<img src="img/gandalf.jpg" >';
+	$contraseña=$_POST["contraseña"];
+	$usuario=$_POST["usuario"];
 
-if ($user==$usuario & $pasword==$contraseña)
- {
-	$img= '<img src="img/yisus.jpg"';	
-}
+	if ($user==$usuario & $pasword==$contraseña)
+ 		{
+			$img= '<img src="img/yisus.jpg"';	
+		}
 	echo $img;
 
 
  ?>
-	
+</center>	
 </body>
 </html>
 	
