f908f2e8be9f26fed03dcba83fe5637def739b33
jerevallejo <44449584+jerevallejo@users.noreply.github.com>
2018-10-25 17:40:52 -0300

    Add files via upload

---
+++ CursoPhp-master/login/validadorLogin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<center>
+
+<?php
+	$pasword="admin";
+	$user="admin"; 
+	$img='<img src="img/gandalf2.png">';
+	$contraseña=$_POST["contraseña"];
+	$usuario=$_POST["usuario"];
+
+	if ($user==$usuario & $pasword==$contraseña)
+ 		{
+			$img= '<img src="img/yisus.jpg">';
+		}
+	echo $img;
+
+
+ ?>
+</center>	
+</body>
+</html>
+	2
