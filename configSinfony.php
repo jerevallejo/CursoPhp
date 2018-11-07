d06ed40b22f5f881cdebeb2729fd0a91e05a683f
jerevallejo <44449584+jerevallejo@users.noreply.github.com>
2018-11-07 17:17:43 -0300

    Add files via upload

--- configSinfony.php
+++ configSinfony.php
@@ -1,9 +1,15 @@
 http//
 tengo el archivo que tnego que pegar en el dico C en C:\xampp\php\
-;aca pegamos la linea para configrar sinfony y descargarlo en nuestra carpeta en nuestra linea 1886
+;aca pegamos la linea para configrar sinfony y descargarlo en nuestra carpeta en nuestra linea 1885
 curl.cainfo = C:\xampp\php\cacert.pem
-desde la consola ejecuto esport PATH="$PATH:/c/xampp/php"
+desde la consola ejecuto -> export PATH="$PATH:/c/xampp/php"
+php -v muestra la version que tengo de php
 luego ejecuto-> php -r "file_put_contents('symfony', file_get_contents('https://symfony.com/installer'));"
 luego creo un-> php symfony new proyecto 2.8 
 lugo cd proyecto
 $ls (para listar archivos)
+
+
+dentro de la carpeta proyecto 
+ejecutar php app/console (te muestra los comandos)
+ejecutar php app/console start:server &
