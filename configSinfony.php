ed39662d2a689e69fa2fe066fc32677fca257071
jerevallejo <44449584+jerevallejo@users.noreply.github.com>
2018-11-08 11:04:37 -0300

    Update configSinfony.php

--- configSinfony.php
+++ configSinfony.php
@@ -1,7 +1,7 @@
-http//
+http://curl.haxx.se/ca/cacert.pem.
 tengo el archivo que tnego que pegar en el dico C en C:\xampp\php\
 ;aca pegamos la linea para configrar sinfony y descargarlo en nuestra carpeta en nuestra linea 1885
-curl.cainfo = C:\xampp\php\cacert.pem
+curl.cainfo = C:\xampp\php\cacert.pem ver si va entre ""
 desde la consola ejecuto -> export PATH="$PATH:/c/xampp/php"
 php -v muestra la version que tengo de php
 luego ejecuto-> php -r "file_put_contents('symfony', file_get_contents('https://symfony.com/installer'));"
